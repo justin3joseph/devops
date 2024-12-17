c53f662c35ab5b515e5f25238b14c97669b83218
justin3joseph <justin3joseph@gmail.com>
2024-12-17 03:00:05 -0500

    Initial changes

--- file.c
+++ file.c
@@ -1,16 +1,7 @@
 main()
 {
-function_1()
+function()
   {
   //initial code segment;
   }
-function_2()
-  {
-   // feature 1 added by devA
-   devA added the new feature
-  }
-function_3()
-{
-   //Feature added by devB
-}
 }
